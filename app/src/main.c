956913ef9d4e0cc944ea27760ccadb2f34e51880
wlt <wltwt@proton.me>
2026-02-06 14:09:07 +0100

    Beginning experiments with setting up various modules

---
+++ app/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022 Rodrigo Peixoto <rodrigopex@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Modified for educational purposes.
+ */
+
+#include <stdint.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/zbus/zbus.h>
+LOG_MODULE_DECLARE(zbus, CONFIG_ZBUS_LOG_LEVEL);
+
+
