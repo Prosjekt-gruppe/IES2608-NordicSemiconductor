9df2b8729e4e994e58d4dc6c72cea2a1b1e263c8
Elektromannen <Ottarmr@stud.ntnu.no>
2026-02-27 15:54:46 +0100

    Beginning of I2C implementation

---
+++ Firmware/LDS/src/Config.h
@@ -0,0 +1,11 @@
+
+
+
+//I2C config
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define masterAdr 0x00
+#define slaveAdr 0x01
+
+//Serial config
+#define BAUDRATE 115200
--- Firmware/LDS/src/main.cpp
+++ Firmware/LDS/src/main.cpp
@@ -1,20 +1,32 @@
 #include <Arduino.h>
-#include <SD.h>
 #include <Wire.h>
 #include <WiFi.h>
+#include "Config.h"
 
 //Puts Esp32 into Modem sleep for redused power consumption
-static void ModemSleep(){
-  esp_sleep_disable_wakeup_source;
-  btStop();
-  WiFi.mode(WIFI_OFF);
-}
+static void ModemSleep();
+static void I2C_send();
 
 void setup(){
-  ModemSleep();
-
+	Serial.begin(BAUDRATE);
+	Wire.begin(SCL_PIN,SDA_PIN); //Standard Frequency
+	ModemSleep();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	Wire.onRequest(I2C_send);
+}
+
+
+static void ModemSleep(){
+    esp_sleep_disable_wakeup_source;
+  	btStop();
+  	WiFi.mode(WIFI_OFF);
+}
+
+static void I2C_send(byte data){
+    Wire.beginTransmission(slaveAdr);
+    Wire.write((data, sizeof(data)));
+    Wire.endTransmission();
+    Serial.println("Data sendt");
 }
