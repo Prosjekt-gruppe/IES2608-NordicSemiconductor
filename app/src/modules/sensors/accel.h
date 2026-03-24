fc31d889298b877d1c060f759a3dbcf028e65753
Youmayu <103046178+Youmayu@users.noreply.github.com>
2026-03-24 15:59:09 +0100

    Add header file for accelerometer module with function declaration

---
+++ app/src/modules/sensors/accel.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef APP_MODULES_SENSORS_ACCEL_H_
+#define APP_MODULES_SENSORS_ACCEL_H_
+
+int accel_start(void);
+
+#endif /* APP_MODULES_SENSORS_ACCEL_H_ */
